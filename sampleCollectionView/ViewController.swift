c278c07c820ba2114b87bfd5b0fdf869d8c26d11
ei17ringo <ei17ringo@gmail.com>
2016-02-11 08:25:13 +0800

    commit init

--- sampleCollectionView/ViewController.swift
+++ sampleCollectionView/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UICollectionViewDelegate Protocol
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell:CustomCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCell
+        cell.title.text = "タイトル";
+        cell.image.image = UIImage(named: "berry.png")
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20;
+    }
 
 }
 
