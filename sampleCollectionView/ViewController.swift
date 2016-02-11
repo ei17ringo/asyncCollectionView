5e26e02ece819564749258e51bbd65fc183664fe
ei17ringo <ei17ringo@gmail.com>
2016-02-11 12:14:33 +0800

    API連動

--- sampleCollectionView/ViewController.swift
+++ sampleCollectionView/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate  {
 
+    var musicList:[NSDictionary] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //itunesのAPIからmaroon5の情報を20件取得
+        var url = NSURL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=marron5&limit=20")
+        var request = NSURLRequest(URL:url!)
+        var jsondata = (try! NSURLConnection.sendSynchronousRequest(request, returningResponse: nil))
+        let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata, options: [])) as! NSDictionary
+        for(key, data) in jsonDictionary{
+            //print("\(key)=\(data)")
+            if (key as! String == "results"){
+                var resultArray = data as! NSArray
+                for (eachMusic) in resultArray{
+                
+                    print(eachMusic["artworkUrl100"])
+                    print(eachMusic["trackName"])
+                    var newMusic:NSDictionary = ["name":eachMusic["trackName"] as! String,"image":eachMusic["artworkUrl100"] as! String]
+                    
+                    musicList.append(newMusic)
+                }
+                
+                
+            
+            }
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,8 +50,17 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
     // MARK: - UICollectionViewDelegate Protocol
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell:CustomCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCell
-        cell.title.text = "タイトル";
-        cell.image.image = UIImage(named: "berry.png")
+//        cell.title.text = "タイトル";
+//        cell.image.image = UIImage(named: "berry.png")
+        
+        
+        let url = NSURL(string: musicList[indexPath.row]["image"] as! String);
+        var err: NSError?;
+        let imageData :NSData = (try! NSData(contentsOfURL:url!,options: NSDataReadingOptions.DataReadingMappedIfSafe));
+        var img = UIImage(data:imageData);
+        
+        cell.title.text = musicList[indexPath.row]["name"] as! String;
+        cell.image.image = img
         cell.backgroundColor = UIColor.blackColor()
         return cell
     }
