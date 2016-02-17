7403e2997f35613d43ddd5400ac2a754783b6747
ei17ringo <ei17ringo@gmail.com>
2016-02-17 15:34:11 +0800

    commit init

--- sampleCollectionView/ViewController.swift
+++ sampleCollectionView/ViewController.swift
@@ -10,34 +10,48 @@ import UIKit
 
 class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate  {
 
+    let queue:dispatch_queue_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+
     var musicList:[NSDictionary] = []
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myCollectionView.hidden = true
         
-        
-        //itunesのAPIからmaroon5の情報を20件取得
-        var url = NSURL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=marron5&limit=20")
-        var request = NSURLRequest(URL:url!)
-        var jsondata = (try! NSURLConnection.sendSynchronousRequest(request, returningResponse: nil))
-        let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata, options: [])) as! NSDictionary
-        for(key, data) in jsonDictionary{
-            //print("\(key)=\(data)")
-            if (key as! String == "results"){
-                var resultArray = data as! NSArray
-                for (eachMusic) in resultArray{
-                
-                    print(eachMusic["artworkUrl100"])
-                    print(eachMusic["trackName"])
-                    var newMusic:NSDictionary = ["name":eachMusic["trackName"] as! String,"image":eachMusic["artworkUrl100"] as! String]
-                    
-                    musicList.append(newMusic)
-                }
-                
-                
+        dispatch_async(queue) {() -> Void in
+            // 別スレッドでの処理
+            //itunesのAPIからmaroon5の情報を20件取得
+            var url = NSURL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=maroon5&limit=20")
+            var request = NSURLRequest(URL:url!)
+            var jsondata = (try! NSURLConnection.sendSynchronousRequest(request, returningResponse: nil))
+            let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata, options: [])) as! NSDictionary
             
+            //APIから取得してきたデータの中で使用するものだけを、newMusicに保存
+            
+            for(key, data) in jsonDictionary{
+                //print("\(key)=\(data)")
+                if (key as! String == "results"){
+                    var resultArray = data as! NSArray
+                    for (eachMusic) in resultArray{
+                        
+                        print(eachMusic["artworkUrl100"])
+                        print(eachMusic["trackName"])
+                        var newMusic:NSDictionary = ["name":eachMusic["trackName"] as! String,"image":eachMusic["artworkUrl100"] as! String]
+                        
+                        //配列のプロパティに追加
+                        self.musicList.append(newMusic)
+                        
+                    }
+                    
+                }
             }
+            
+            self.myCollectionView.reloadData()
+            self.myCollectionView.hidden = false
         }
+
 
         
     }
@@ -49,11 +63,12 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
 
     // MARK: - UICollectionViewDelegate Protocol
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
         let cell:CustomCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCell
 //        cell.title.text = "タイトル";
 //        cell.image.image = UIImage(named: "berry.png")
         
-        
+        // URLを元に画像データを取得
         let url = NSURL(string: musicList[indexPath.row]["image"] as! String);
         var err: NSError?;
         let imageData :NSData = (try! NSData(contentsOfURL:url!,options: NSDataReadingOptions.DataReadingMappedIfSafe));
@@ -70,7 +85,7 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20;
+        return musicList.count;
     }
 
 }
